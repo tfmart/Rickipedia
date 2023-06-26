171fe5dfd27cd93149bf610633635382e5e4964b
Tomás Martins <tomas.marti@me.com>
2023-06-26 17:44:39 -0300

    Mock all characters service

---
+++ RickipediaTests/Sources/CharacterListViewModelTests.swift
@@ -0,0 +1,21 @@
+//
+//  CharacterListViewModelTests.swift
+//  RickipediaTests
+//
+//  Created by Tomas Martins on 26/06/23.
+//
+
+import XCTest
+@testable import Rickipedia
+
+final class CharacterListViewModelTests: XCTestCase {
+    func testFetchingFirstPage() async throws {
+        let viewModel = CharacterListViewModel()
+        viewModel.service = MockAllCharactersService()
+        XCTAssertEqual(viewModel.currentPage, 1)
+        XCTAssertTrue(viewModel.characters.isEmpty)
+        await viewModel.fetchCharacters()
+        XCTAssertFalse(viewModel.characters.isEmpty)
+        XCTAssertEqual(viewModel.currentPage, 2)
+    }
+}
