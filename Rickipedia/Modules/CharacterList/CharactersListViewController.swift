d2d4a4abe1d90a2bf82e3b86903ee81b50da48c9
Tomás Martins <tomas.marti@me.com>
2023-06-30 11:28:49 -0300

    Refactored list view controller to use DiffableDataSource

--- Rickipedia/Modules/CharacterList/CharactersListViewController.swift
+++ Rickipedia/Modules/CharacterList/CharactersListViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RKPDesign
 
 protocol CharactersListViewControllerDelegate: AnyObject {
     func didSelectCharacter(_ character: Character)
@@ -17,6 +18,11 @@ class CharactersListViewController: UIViewController {
     private let searchController = UISearchController(searchResultsController: nil)
 
     weak var delegate: CharactersListViewControllerDelegate?
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Character>!
+    
+    enum Section {
+        case main
+    }
     
     init(viewModel: CharactersListViewModel) {
         self.viewModel = viewModel
@@ -26,7 +32,6 @@ class CharactersListViewController: UIViewController {
         self.collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         super.init(nibName: nil, bundle: nil)
         
-        setupCollectionView()
         let menu = setupFilterButton()
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: .init(systemName: "line.3.horizontal.decrease.circle"), menu: menu)
     }
@@ -39,17 +44,19 @@ class CharactersListViewController: UIViewController {
         super.viewDidLoad()
         self.title = "Characters"
         self.view.backgroundColor = .systemBackground
+        setupCollectionView()
+        configureDataSource()
+        
         Task {
             await viewModel.fetchCharacters()
-            self.collectionView.reloadData()
+            applySnapshot()
         }
     }
     
     private func setupCollectionView() {
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        collectionView.register(CharacterCell.self, forCellWithReuseIdentifier: CharacterCell.reuseIdentifier)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(RKPCharacterCell.self, forCellWithReuseIdentifier: RKPCharacterCell.reuseIdentifier)
+        collectionView.delegate = self
         
         searchController.searchResultsUpdater = self
         searchController.obscuresBackgroundDuringPresentation = false
@@ -58,7 +65,6 @@ class CharactersListViewController: UIViewController {
         navigationItem.searchController = searchController
         definesPresentationContext = true
 
-        
         view.addSubview(collectionView)
         
         NSLayoutConstraint.activate([
@@ -103,26 +109,38 @@ class CharactersListViewController: UIViewController {
             await self.viewModel.searchCharacters(searchController.searchBar.text, status: statusToApply)
             let menu = self.setupFilterButton()
             self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: .init(systemName: "line.3.horizontal.decrease.circle"), menu: menu)
-            self.collectionView.reloadData()
+            applySnapshot()
         }
+    }
+    
+    private func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Character>(collectionView: collectionView) { collectionView, indexPath, character in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RKPCharacterCell.reuseIdentifier, for: indexPath) as? RKPCharacterCell else {
+                fatalError("Failed to dequeue CharacterCell")
+            }
+            
+            cell.configure(with: character.name, imageURL: URL(string: character.image))
+            cell.accessories = [.disclosureIndicator()]
+            
+            return cell
+        }
+    }
+    
+    private func applySnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Character>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(viewModel.characters, toSection: .main)
+        dataSource.apply(snapshot, animatingDifferences: true)
     }
 }
 
-extension CharactersListViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModel.characters.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CharacterCell.reuseIdentifier, for: indexPath) as? CharacterCell else {
-            fatalError("Failed to dequeue CharacterCell")
+extension CharactersListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text else { return }
+        Task {
+            await viewModel.searchCharacters(searchText, status: viewModel.currentStateFilter)
+            applySnapshot()
         }
-        
-        let character = viewModel.characters[indexPath.item]
-        cell.configure(with: character)
-        cell.accessories = [.disclosureIndicator()]
-        
-        return cell
     }
 }
 
@@ -132,23 +150,13 @@ extension CharactersListViewController: UICollectionViewDelegate {
         if indexPath.item == lastItem && viewModel.hasNextPage {
             Task {
                 await viewModel.fetchCharacters()
-                collectionView.reloadData()
+                applySnapshot()
             }
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let character = viewModel.characters[indexPath.item]
+        guard let character = dataSource.itemIdentifier(for: indexPath) else { return }
         delegate?.didSelectCharacter(character)
-    }
-}
-
-extension CharactersListViewController: UISearchResultsUpdating {
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let searchText = searchController.searchBar.text else { return }
-        Task {
-            await viewModel.searchCharacters(searchText, status: viewModel.currentStateFilter)
-            collectionView.reloadData()
-        }
     }
 }
